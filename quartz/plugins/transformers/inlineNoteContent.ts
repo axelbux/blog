c368e4429ea289e144f8b6be6c7fd51eb895d9dc
Axel Bux <axel999x@gmail.com>
2025-11-03 19:58:16 +0300

    Sync: 2025-11-03 19:58

---
+++ quartz/plugins/transformers/inlineNoteContent.ts
@@ -0,0 +1,5 @@
+const allowedExtensions = new Set([
+  ".png", ".jpg", ".jpeg", ".gif", ".webp", ".svg",
+  ".pdf", ".mp4", ".mp3", ".wav", ".ogg", ".zip", ".epub", ".m4a",
+  ".ps1", ".bat", ".txt", ".json", ".csv", ".md"
+])
