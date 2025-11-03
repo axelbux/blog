15514ec238ebf1a4461d7635a1f84be0fb6255f1
Axel Bux <axel999x@gmail.com>
2025-11-03 21:16:26 +0300

    Update componentResources.ts

--- quartz/plugins/emitters/componentResources.ts
+++ quartz/plugins/emitters/componentResources.ts
@@ -106,7 +106,21 @@ function addGlobalPageResources(ctx: BuildCtx, componentResources: ComponentReso
       
       document.head.appendChild(gtagScript);
     `)
-  } else if (cfg.analytics?.provider === "plausible") {
+  } else if (cfg.analytics?.provider === "yandex") {
+componentResources.afterDOMLoaded.push(`
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();
+for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(105100344, "init", {
+     clickmap:true,
+     trackLinks:true,
+     accurateTrackBounce:true
+});
+`)
+} else if (cfg.analytics?.provider === "plausible") {
     const plausibleHost = cfg.analytics.host ?? "https://plausible.io"
     componentResources.afterDOMLoaded.push(`
       const plausibleScript = document.createElement('script');
