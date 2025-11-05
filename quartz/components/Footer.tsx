5aa22240eee37eee6ec20053fc46c0245ac8ade7
Axel Bux <axel999x@gmail.com>
2025-11-05 21:04:17 +0300

    Правка стилей 2 end mobile4

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -12,23 +12,25 @@ export default ((opts?: Options) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
-      <footer class={`${displayClass ?? ""}`}>
+   <footer class={`${displayClass ?? ""}`}>
         
-      <p>  
-	  <ul>
+      
+		<ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
               <a href={link}>{text}</a>
-            </li>
+            </li>		
           ))}
-        |<span style="font-size:0.8em; text-align:right;">
-		<li>
+			<li>
+              <a href="https://www.youtube.com/@axelbux">Мой YouTube</a>
+            </li>
+        </ul>
+		
+		<p style="font-size:0.8em; text-align: center;">
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-		  </li>
-		  </span> 
-        </ul>
-		</p>
+		  </p>
+
       </footer>
     )
   }
